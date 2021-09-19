e82d5e5b49fdc54bec0cf30fdf969e84d9aaf2c6
Diana Obiedkova <missdee31102001@gmail.com>
2021-09-19 23:28:12 +0300

    initial entities

---
+++ Quoridor/Quoridor/Models/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quoridor.Models
+{
+    public class Player: Entity
+    {
+        public string Name { get; set; }
+        public Position position { get; set; }
+
+        public Player(Position position)
+        {
+            this.position = position;
+            Name = "Player " + Id;
+        }
+
+        public Player(string name, Position position)
+        {
+            this.position = position;
+            Name = name;
+        }
+    }
+}

5e4e4dbacd5aa3be6497ec0b938226725812489d
Diana Obiedkova <missdee31102001@gmail.com>
2021-09-20 16:41:16 +0300

    game class

---
+++ Quoridor/Quoridor/Models/Quoridor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quoridor.Models
+{
+    public class Quoridor
+    {
+
+        private readonly Player firstP;
+        private readonly Player secondP;
+        public Player CurrentP { get; private set; }
+
+        public Quoridor(Player firstP, Player secondP)
+        {
+            this.firstP = firstP;
+            this.secondP = secondP;
+        }
+
+        public void SwitchPlayers()
+        {
+            CurrentP = CurrentP == firstP ? secondP : firstP;
+        }
+    }
+}

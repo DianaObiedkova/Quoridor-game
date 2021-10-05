09310bddbdb4769613ac936c667ddd4f4f3ce270
cyberg00se <zhuk.bohdana@gmail.com>
2021-10-06 01:23:54 +0300

    updated Dijkstra

--- Quoridor/Quoridor/Models/Dijkstra.cs
+++ Quoridor/Quoridor/Models/Dijkstra.cs
@@ -49,6 +49,9 @@ namespace Quoridor.Models
             edges = graphEdges;
         }
 
+        //start.weight = 0
+        //othervertices[index].weight = INT_MAX
+        //allvertices[index].IsChecked = false
         public static int FindShortestPathLength(Vertex start)
         {
             if (this.vertices.Count() == 0 || this.edges.Count() == 0)
@@ -56,12 +59,61 @@ namespace Quoridor.Models
                 throw new Exception("dijkstra data error");
             }
             startVertex = start;
-            IterateDijkstra(startVertex);
 
+            //первая итерация
+            IterateDijkstra(startVertex);
+            //в цикл foreach vert in vertices
+                //Vertex nextVertex = ближайшая к ней unchecked // = GetNearestUnchecked()
+                /*if(nextVertex != null)
+                {
+                    IterateDijkstra(nextVertex);
+                }
+                else
+                    break;
+                */
+                //if all vertices are checked, exit/break
         }
-        public void IterateDijkstra()
+        public void IterateDijkstra(Vertex currentVertex)
         {
+            //todo:
+            //получить neigbours = GetNeighbours(currentVertex)
+            //пройтись по ним foreach item in neigbours:
+            //  if(item.IsChecked==false)
+            //  {
+            //      int newvalue = currentVertex.value + GetEdge(item, currentVertex).weight
+            //      если item.value > newvalue
+            //          то назначить для item.value = newwalue, item.prevVertex = currentVertex
+            //      иначе else {} //ничего не делать
+            //  }
             
+            //в конце
+            currentVertex.IsChecked = true;
         }
+
+        //todo: метод для получения списка соседей GetNeighbours(Vertex current): 
+        //  Concat списков (secondVertex где у ребер firstVertex==current)+(firstVertex где у ребер secondVertex==current)
+
+        //todo: поиск ребра между двумя вершинами GetEdge(Vertex first, Vertex second) returns Edge
+
+        /*private Vertex GetNearestUnchecked()
+        {
+            //список Vertex unchecked = выбрать vert из this.vertices где vert.IsChecked == false
+            if(unchecked.Count != 0)
+            {
+                Vertex minVertex = unchecked[0]; // нужен первый элемент
+                int minValue = unchecked[0].value; //нужен первый элемент
+                foreach (Vertex vert in unchecked)
+                {
+                    if(vert.value < minValue)
+                    {
+                        minValue = vert.value;
+                        minVertex = vert;
+                    }
+                }
+                return minVertex;
+            }
+            else
+                return null;
+        }*/
     }
 }

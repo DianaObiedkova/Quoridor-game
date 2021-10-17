eb14491844b07e47d86331c77e00cf33df787e4c
cyberg00se <zhuk.bohdana@gmail.com>
2021-10-17 23:23:26 +0300

    MovePawn action

--- Quoridor/Quoridor/Controllers/QuoridorController.cs
+++ Quoridor/Quoridor/Controllers/QuoridorController.cs
@@ -41,8 +41,40 @@ namespace Quoridor.Controllers
             return PartialView("PartialMoves", Game.PossibleMovePawn());
         }
 
-        public IActionResult MovePawn(Direction direction)
+        public IActionResult MovePawn(string new_cellname)
         {
+            string cur_cellname = Game.CurrentP.Pawn.Cell.Name;
+            Direction direction = Direction.North;
+            if(((byte)cur_cellname[1]) == ((byte)new_cellname[1]))
+            {
+                if(((byte)cur_cellname[0]) > ((byte)new_cellname[0]))
+                    direction = Direction.West;
+                else if(((byte)cur_cellname[0]) < ((byte)new_cellname[0]))
+                    direction = Direction.East;
+                else{}
+            }
+            else if(((byte)cur_cellname[1]) > ((byte)new_cellname[1]))
+            {
+                if(((byte)cur_cellname[0]) > ((byte)new_cellname[0]))
+                    direction = Direction.NorthWest;
+                else if(((byte)cur_cellname[0]) < ((byte)new_cellname[0]))
+                    direction = Direction.NorthEast;
+                else
+                    direction = Direction.North;
+            }
+            else if(((byte)cur_cellname[1]) < ((byte)new_cellname[1]))
+            {
+                if(((byte)cur_cellname[0]) > ((byte)new_cellname[0]))
+                    direction = Direction.SouthWest;
+                else if(((byte)cur_cellname[0]) < ((byte)new_cellname[0]))
+                    direction = Direction.SouthEast;
+                else
+                    direction = Direction.South;
+            }
+            else
+            {
+                return BadRequest();
+            }
             Game.MovePawn(direction);
             return Ok();
         }
